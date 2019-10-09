cdc1b5c2bc925267ece7a7967b7fc31bd924c49f
Charity Parks <tuppercharity@gmail.com>
2019-10-09 02:46:04 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,6 +7,12 @@ class Mp3_importer
     @path = path 
   end
   
-  
+  def files
+       Dir.entries(@path).select {|entry| entry.include?(".mp3")} 
+    end
+
+    def import
+        files.each {|file| Song.new_by_filename(file)}
+    end
   
 end
